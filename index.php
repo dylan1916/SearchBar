74a421d867e60bcdc584da05345f38833ab351a0
Tordjman <dylantordjman@MacBook-Air-de-Tordjman.local>
2019-09-19 18:28:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$bdd = new PDO('mysql:host=localhost;dbname=searchbar;charset=utf8', 'root', 'root');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SearchBar</title>
+</head>
+<body>
+    
+</body>
+</html>
