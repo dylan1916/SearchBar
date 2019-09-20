a3fbd0cae2de450df37a95b99e79556e80d12338
Tordjman <dylantordjman@MacBook-Air-de-Tordjman.local>
2019-09-20 12:37:30 +0200

    searchBar op

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 $bdd = new PDO('mysql:host=localhost;dbname=searchbar;charset=utf8', 'root', 'root');
 
+$login = $bdd->query('SELECT login FROM users ORDER BY id DESC');
+
+if (isset($_GET['q']) AND !empty($_GET['q']))
+{
+    $q = htmlspecialchars($_GET['q']);
+
+    // MODIFIER LA REQUETE AU CAS OU ON VU UN CHAMPS PLUS LARGE CHOIX
+
+    $login = $bdd->query('SELECT login FROM users WHERE login LIKE "'.$q.'%" ORDER BY id DESC');
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +24,28 @@ $bdd = new PDO('mysql:host=localhost;dbname=searchbar;charset=utf8', 'root', 'ro
     <title>SearchBar</title>
 </head>
 <body>
+
+<form method="GET" action="">
+    <input type="search" name="q" placeholder="Recherche...">
+    <input type="submit" value="Valider">
+<form>
+
+<?php if ($login->rowCount() > 0) { ?>
+    
+    <ul>
+        <?php while ($l = $login->fetch()) { ?>
+            <li><?= $l['login']; ?></li>
+        <?php } ?>
+    </ul>
+
+<?php }else { ?>
+
+    Aucun résultat...
+
+<?php } ?>
+
+
+
     
 </body>
 </html>
